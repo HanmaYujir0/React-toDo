eece1c3a984787780fc9e067dba1045ad7202c1d
Magomed-Emin <sadikov.magomed@bk.ru>
2022-06-20 15:58:49 +0300

    отформатировал при помощи prettier

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import style from './header.module.css'
-
+import style from "./header.module.css";
 
 export default function Header() {
   return (
-  <div className={style.ToDos_header}>
-    <h1>Список дел</h1>
-    <hr />
-  </div>
-  )
+    <div className={style.ToDos_header}>
+      <h1>Список дел</h1>
+      <hr />
+    </div>
+  );
 }
